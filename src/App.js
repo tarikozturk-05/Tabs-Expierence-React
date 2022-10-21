ec4bcfcec079c0e11dbedd038f7f8af6e2a7eff6
Tarik <a.tarik2005@gmail.com>
2022-10-21 12:30:24 +0300

    card yapısı tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+
+
 function App() {
-  return <h2>tabs project setup</h2>
+
+  const url = 'https://course-api.com/react-tabs-project'
+  const [getUser, setGetUser] = useState([])
+  const [desc, setDesc] = useState(0)
+
+    const getApi=async()=>{
+      try {
+        const {data} = await axios(url)
+        // console.log(data);
+        setGetUser(data)
+        
+      } catch (error) {
+        console.log(error);
+      }
+     
+
+    }
+    useEffect(() => {
+      getApi()
+    
+    }, [])
+
+  
+    
+
+
+
+const{company,dates,duties,title}=getUser[desc]
+  return(
+    <section className='section'>
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+    
+      <div className="jobs-center">
+        <div  className="btn-container">
+          {
+            getUser.map((item,index)=>{return(<button className={`job-btn ${index === desc && 'active-btn'}`} onClick={setDesc(index)}>{item.company}</button>)})
+          }
+        </div>
+
+      
+
+
+          <article className='job-info'>
+              <h3>{title}</h3>
+              <h4>{company}</h4>
+              <p className='job-date'>{dates}</p>
+              {duties.map((dut,index)=>{return(
+                <div className='job-desc' key={index}><FaAngleDoubleRight className='job-icon'/> <p>{dut}</p></div>
+              )})}
+          </article>
+
+          </div>
+
+    </section>
+
+  )
 }
 
 export default App
